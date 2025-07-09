a7eb5051ad823344a7eb620dddeeb49ec51e35ce
jeongminb933 <65400179+jeongminb933@users.noreply.github.com>
2025-07-09 23:04:05 +0900

    공격했을때 폭발 파티클 구현
    todo : 프리팹 변형, 적함선 변형

--- GalaxyStrike/Assets/Scripts/Enemy.cs
+++ GalaxyStrike/Assets/Scripts/Enemy.cs
@@ -2,13 +2,17 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public int hitCount = 0;
+    [SerializeField] GameObject destroyedVFX;
+    private int hitCount = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnParticleCollision(GameObject other)
     {
         hitCount++;
+        GameObject Dparticle = Instantiate(destroyedVFX, transform.position, Quaternion.identity);
+
         if(hitCount == 3)
         {
+            Dparticle.GetComponent<ParticleSystem>().Play();
             Destroy(this.gameObject);
             hitCount = 0;
         }
