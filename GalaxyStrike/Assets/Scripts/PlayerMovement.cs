87d58a4a9026eaac2a275252d0171be65ad42cc3
jeongminb933 <65400179+jeongminb933@users.noreply.github.com>
2025-07-09 18:15:58 +0900

    많은일이있었어유ㅠ

---
+++ GalaxyStrike/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float controlSpeed = 10f;
+    [SerializeField] float xClampRange = 5f;
+    [SerializeField] float yClampRange =  3f;
+
+    [SerializeField] float controlRollFactor = 45f;
+    Vector2 movement;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {   
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+
+    public void OnMove(InputValue Value)
+    {
+        movement = Value.Get<Vector2>();
+    }
+    private void ProcessTranslation()
+    {
+        float xOffset = movement.x * controlSpeed * Time.deltaTime;
+        float rawXpos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXpos, -xClampRange, xClampRange);
+
+        float yOffset = movement.y * controlSpeed * Time.deltaTime;
+        float rawYpos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYpos, -yClampRange, yClampRange);
+
+        transform.localPosition = new Vector3(clampedXPos,
+            clampedYPos, 0f);
+    }
+    private void ProcessRotation()
+    {
+        Quaternion targetRotation = Quaternion.Euler(0f, 0f, -controlRollFactor * movement.x);
+        transform.localRotation = targetRotation;
+    }
+}
